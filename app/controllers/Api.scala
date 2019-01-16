708a7415837c133d6070e5975d18357aa0a5cb7a
bleedingtiger <tegre1jl@hotmail.fr>
2019-01-16 09:21:05 +0100

    First step of Epic 2

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -1,13 +1,14 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc.ControllerComponents
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.json.Reads._
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -17,7 +18,7 @@ import scala.concurrent.{ExecutionContext, Future}
 class Api @Inject()(cc: ControllerComponents, ws: WSClient, ec: ExecutionContext) extends AbstractController(cc) {
 
   implicit val execCont: ExecutionContext = ec
-  val githubGraphQLToken = "5e3126eee18c0ee160caff32b2c8e563b7a148d5"
+  val githubGraphQLToken = "f36b5d5274d4e9dc73d57d4cededd9d18b1a8d55"
 
   case class Author(name: String, email: String)
   case class Commiter(author: Author, commits: Int)
@@ -31,32 +32,31 @@ class Api @Inject()(cc: ControllerComponents, ws: WSClient, ec: ExecutionContext
   }
 
 
-  def getTopCommiters(repoName: String, repoOwner: String) = Action.async {
+  def getTopCommiters(repoOwner: String, repoName: String) = Action.async {
     val url = "https://api.github.com/graphql"
 
-    // TODO : More elegant way to represent the GraphQL query
     val data = Json.obj(
       "query" -> {
-        "query {" +
-            "repository(name: " + repoName + ", owner: " + repoOwner + ") {" +
-              "defaultBranchRef {" +
-                "target {" +
-                  "... on Commit {" +
-                    "history(first: 100) {" +
-                      "edges {" +
-                        "node {" +
-                          "author {" +
-                            "name \n" +
-                            "email" +
-                          "}" +
-                        "}" +
-                      "}" +
-                    "}" +
-                  "}" +
-                "}" +
-              "}" +
-            "}" +
-          "}"
+        s"""query {
+            repository(name: $repoName, owner: $repoOwner) {
+              defaultBranchRef {
+                target {
+                  ... on Commit {
+                    history(first: 100) {
+                      edges {
+                        node {
+                          author {
+                            name \n
+                            email
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }"""
       }
     )
     val futureResponse: Future[JsValue] = ws.url(url).addHttpHeaders(("Authorization", "Bearer " + githubGraphQLToken)).post(data).map {
@@ -74,4 +74,76 @@ class Api @Inject()(cc: ControllerComponents, ws: WSClient, ec: ExecutionContext
     })
   }
 
+
+  def getTopLanguages(userName: String) = Action.async {
+    val url = "https://api.github.com/graphql"
+
+    val data = Json.obj(
+      "query" -> {
+        s"""query {
+          user(login: $userName) {
+            repositories(first: 100, isFork: false) {
+              nodes {
+                name
+              }
+            }
+          }
+        }"""
+      }
+    )
+    val futureRepoNames: Future[WSResponse] = ws.url(url).addHttpHeaders(("Authorization", "Bearer " + githubGraphQLToken)).post(data)
+
+    val futureRepoNameList: Future[List[String]] = futureRepoNames.map {
+      res => {
+        val nodes = (res.json \ "data" \ "user" \ "repositories" \ "nodes") \\ "name"
+        val repoNames = nodes.map(repo => repo.as[String])
+        repoNames.toList
+      }
+    }
+    /*val futureList: Future[List[Future[WSResponse]]] = futureRepoNameList.map {
+      res => {
+        res.map(repoName => {
+          val urlBis = s"https://api.github.com/repos/$userName/$repoName/languages"
+          ws.url(urlBis).get()
+        })
+      }
+    }
+
+    val futureFinal = futureList.map {
+      res => {
+        res.map(f => f.map(r => println(r.json)))
+      }
+    }*/
+
+
+    /*val futureList: Future[List[WSResponse]] =
+    for {
+      repoName <- futureRepoNameList
+      languages <- Future.sequence {
+        val urlBis = s"https://api.github.com/repos/$userName/$repoName/languages"
+        ws.url(urlBis).get()
+      }
+    } yield languages
+
+
+    val futureResult: Future[Result] = futureList.map {
+      res => {
+        //println(res)
+        Ok("test")
+      }
+    }
+    futureResult*/
+
+    Future { Ok("TODO") }
+
+
+
+    /*val futureLanguages: Future[Future[WSResponse]] = futureRepoNameList.map {
+      res => {
+        val urlBis = s"https://api.github.com/repos/$userName/$res/languages"
+        ws.url(urlBis).get()
+      }
+    }
+    val flattenFuture = futureLanguages.flatten.map { res => println(res.json)}*/
+  }
 }
